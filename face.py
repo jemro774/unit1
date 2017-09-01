bcf3a8f672421cd2eb3bfd7591f7ad5a5b718474
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-09-01 14:37:38 -0400

    Updated from Brython Server: 9/1/2017 2:37:37 PM

--- face.py
+++ face.py
@@ -1,1 +1,11 @@
+#Jack Robey
+#9/1/17
+#face.py - create a face
+
+print('   xxxxx   ')
+print('  x     x  ')
+print('((  o o  ))')
+print('  |  v  |  ')
+print('  | === |  ')
+print('   -----   ')
 
