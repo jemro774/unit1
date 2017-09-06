7e0e6b9f16d24e27f9d989d5d66a61f7edab09cd
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-09-06 14:01:30 -0400

    Updated from Brython Server: 9/6/2017 2:01:29 PM

--- nameAge.py
+++ nameAge.py
@@ -1,1 +1,9 @@
+#Jack Robey
+#9/6/17
+#nameAge.py - asks a user for their first and last name and then their age
+
+name = input('Enter your first and last name: ')
+age = input('Enter your age: ')
+firstName, lastName = name.split()
+print(firstName)
 
