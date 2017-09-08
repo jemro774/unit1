4ece1889c189782769baa853893025e49149e76d
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-09-08 15:29:37 -0400

    Updated from Brython Server: 9/8/2017 3:29:37 PM

--- slope.py
+++ slope.py
@@ -1,8 +1,12 @@
 #Jack Robey
 #9/8/17
-#slope.py - calculates the slope of line given two points
+#slope.py - calculates the slope of a line given two points
 
-x1 = float(input('Enter the x-value for your first coordinate: ')
-y1 = float(input('Enter the y-value for your first coordinate: ')
-x2 = float(input('Enter the x-value for your second coordinate: ')
-y1 = float(input('Enter the y-value for your second coordinate: ')
+x1 = float(input('Enter the x-value for your first coordinate: '))
+y1 = float(input('Enter the y-value for your first coordinate: '))
+x2 = float(input('Enter the x-value for your second coordinate: '))
+y2 = float(input('Enter the y-value for your second coordinate: '))
+slope = round((y2-y1)/(x2-x1), 2)
+print('The slope is', slope)
+b = round(-1*slope*x1+y1, 2)
+print('The equation of the line is y =', slope, 'x +', b) 
