adcf335a8e78a0ebfe32fd648ee8eba9a76b32f9
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-09-08 14:35:04 -0400

    Update nameAge.py

--- nameAge.py
+++ nameAge.py
@@ -1,9 +1,14 @@
 #Jack Robey
 #9/6/17
-#nameAge.py - asks a user for their first and last name and then their age
+#nameAge.py - prints how many characters are in each name entered and how old the person will be next year based on their entered age
 
 name = input('Enter your first and last name: ')
-age = input('Enter your age: ')
+age = int(input('Enter your age: '))
 firstName, lastName = name.split()
-print(firstName)
+charactersFirst = len(firstName)
+print('There are', charactersFirst, 'characters in your first name')
+chracterSecond = len(lastName)
+print('There are', chracterSecond, 'characters in your last name')
+nextAge = age + 1
+print('Next year you will be', nextAge, 'years old')
 
