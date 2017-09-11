0cd72b6d28fd52fa8acc62ebcb30b2c3babbe414
jemro774 <31485871+jemro774@users.noreply.github.com>
2017-09-11 14:34:41 -0400

    Updated from Brython Server: 9/11/2017 2:34:40 PM

--- quiz1.py
+++ quiz1.py
@@ -1,1 +1,13 @@
+#Jack Robey
+#9/11/17
+#quiz1.py - first quiz for computer programming
 
+print('Jack Robey')
+num1 = int(input('What is your first number? '))
+num2 = int(input('What is your second number? '))
+numSum = num1 + num2
+numProduct = num1 * num2
+print('The sum is', numSum)
+print('The product is', numProduct)
+luckyNum = num1 * 7 + num2 * 7
+print('Your lucky number is', luckyNum)
